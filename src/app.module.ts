f5ce3adb65be43913bd3344f99518e6e4521274d
laatu08 <partha2002borah@gmail.com>
2025-02-13 22:10:50 +0530

    Throttle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CronJobModule } from './cron-job/cron-job.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://partha2002borah:S0wJJQjonNpllJUi@cluster0.6lnbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
-    CronJobModule
+    CronJobModule,
+    ThrottlerModule.forRoot([{
+      ttl:60000,
+      limit:10,
+    }])
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/cron-job/cron-job.controller.ts
+++ src/cron-job/cron-job.controller.ts
@@ -1,32 +1,39 @@
-import { Controller, Get, Post, Put, Body, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Delete, Param, UseGuards } from '@nestjs/common';
 import { CronJobService } from './cron-job.service';
 import { CronJob } from './cron-job.schema';
+import { Throttle,ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('cron-jobs')
+@UseGuards(ThrottlerGuard)
 export class CronJobController {
   constructor(private readonly cronJobService: CronJobService) {}
 
   @Post()
+  @Throttle({default:{limit:5, ttl:60000}})
   create(@Body() cronJob: Partial<CronJob>) {
     return this.cronJobService.create(cronJob);
   }
 
   @Get()
+  @Throttle({default:{limit:10, ttl:60000}})
   getAll() {
     return this.cronJobService.getAll();
   }
 
   @Get(':id')
+  @Throttle({default:{limit:10, ttl:60000}})
   getById(@Param('id') id: string) {
     return this.cronJobService.getById(id);
   }
 
   @Put(':id')
+  @Throttle({default:{limit:3, ttl:60000}})
   update(@Param('id') id: string, @Body() cronJob: Partial<CronJob>) {
     return this.cronJobService.update(id, cronJob);
   }
 
   @Delete(':id')
+  @Throttle({default:{limit:3, ttl:60000}})
   delete(@Param('id') id: string) {
     return this.cronJobService.delete(id);
   }
